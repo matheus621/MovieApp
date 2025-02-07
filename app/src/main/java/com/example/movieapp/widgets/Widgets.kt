b91b48d97b0e179e717837d16d72bcd7fb4b3267
Matheus Souza <matheus621cardoso@gmail.com>
2025-02-07 17:38:27 -0300

    updating project

--- app/src/main/java/com/example/movieapp/widgets/Widgets.kt
+++ app/src/main/java/com/example/movieapp/widgets/Widgets.kt
@@ -15,9 +15,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
 import com.example.movieapp.model.Movie
 import com.example.movieapp.model.getMovies
@@ -32,7 +36,7 @@ fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) 
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(130.dp)
+            //.height(130.dp)
             .clickable {
                 onItemClick(movie.id)
             },
@@ -61,12 +65,38 @@ fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) 
             }
             Column(modifier = Modifier.padding(4.dp)) {
                 Text(text = movie.title, style = MaterialTheme.typography.h6)
-                Text(text = "Director: ${movie.director}", style = MaterialTheme.typography.caption)
                 Text(text = "Release: ${movie.year}", style = MaterialTheme.typography.caption)
 
                 AnimatedVisibility(visible = expanded) {
-                    Column() {
-                        Text("Hello World")
+                    Column {
+                        Divider(modifier = Modifier.padding(2.dp))
+                        Text(buildAnnotatedString {
+                            withStyle(style = SpanStyle(color = Color.DarkGray, fontSize = 13.sp)) {
+                                append("Plot: ")
+                            }
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray,
+                                    fontSize = 13.sp,
+                                    fontWeight = FontWeight.Light
+                                )
+                            ) {
+                                append(movie.plot)
+                            }
+                        }, modifier = Modifier.padding(6.dp))
+                        Divider(modifier = Modifier.padding(2.dp))
+                        Text(
+                            text = "Director: ${movie.director}",
+                            style = MaterialTheme.typography.caption
+                        )
+                        Text(
+                            text = "Actors: ${movie.actors}",
+                            style = MaterialTheme.typography.caption
+                        )
+                        Text(
+                            text = "Rating: ${movie.rating}",
+                            style = MaterialTheme.typography.caption
+                        )
                     }
                 }
                 Icon(
